f8eafb8c8e7577d99f2415136ba067eb155085dc
Kannan Thambiah <pygospa@gmail.com>
2019-08-22 15:32:43 +0200

    Implement form for adding personal data

--- spec/views/personal_data/new.haml_spec.rb
+++ spec/views/personal_data/new.haml_spec.rb
@@ -3,5 +3,6 @@ require "rails_helper"
 RSpec.describe "personal_data/new.haml" do
   it "displays a form to add personal data" do
     render
+    expect(rendered).to have_css("form.personal-data")
   end
 end
