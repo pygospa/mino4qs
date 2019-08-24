bd929663218eee0817eba1574439f70ba6565215
Kannan Thambiah <pygospa@gmail.com>
2019-08-24 23:09:59 +0200

    Configure rspec, capybara, guard

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,16 @@
+require 'simplecov'
+SimpleCov.start
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+
 require 'rspec/rails'
 require 'database_cleaner'
+require 'capybara/rails'
+
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
@@ -12,12 +19,36 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 
+selenium_url = "http://selenium_chrome:4444/wd/hub"
+
+Capybara.register_driver :selenium_remote do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w(ignore-certificate-errors text-type)}
+  )
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :remote,
+    url: selenium_url,
+    desired_capabilities: capabilities)
+end
+
+
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = false
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+
+  # Capybara
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_remote
+    host! "http://mino_test:3001/"
+  end
+
+  config.include FactoryBot::Syntax::Methods
 
   # database_cleaner
-  config.use_transactional_fixtures = false
-
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
@@ -39,8 +70,4 @@ RSpec.configure do |config|
   config.after(:all) do
     DatabaseCleaner.clean
   end
-
-  config.infer_spec_type_from_file_location!
-
-  config.filter_rails_from_backtrace!
 end
