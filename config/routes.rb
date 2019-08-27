b178a7b38beef789ef830c9f63d5c0071dc57855
Kannan Thambiah <pygospa@gmail.com>
2019-08-27 22:00:16 +0200

    Fix system spec's route error (personal_data#index)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'personal_data/index', to: 'personal_data#index'
 end
