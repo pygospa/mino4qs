fc859b4225580c25c975faa642ea02bdcf1ac045
Kannan Thambiah <kannan.thambiah@neuland-bfi.de>
2019-08-28 12:57:37 +0200

    Form view tests green

--- spec/views/personal_data/new.haml_spec.rb
+++ spec/views/personal_data/new.haml_spec.rb
@@ -2,19 +2,24 @@ require 'rails_helper'
 
 RSpec.describe 'personal_data/new.haml', type: :view do
   it 'displays from for personal data' do
-    render
-    expect(rendered).to have_css("form.personal-data")
+    expect(render).to have_css("form.personal-data")
   end
 
   context 'form' do
     it 'contains field for birthday' do
-      render
-      expect(rendered).to have_field("birthday")
+      expect(render).to have_field("birthday")
     end
 
     it 'contains a field for gender' do
-      render
       expect(render).to have_field("gender")
+    end
+
+    it 'contains a field for height' do
+      expect(render).to have_field("height")
+    end
+
+    it 'contains a button to submit form' do
+      expect(render).to have_button('Submit')
     end
   end
 end
