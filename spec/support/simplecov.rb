9ced4e128d9351a83874bc036ad01eb06e713f29
Kannan Thambiah <kannan.thambiah@neuland-bfi.de>
2019-08-28 13:25:55 +0200

    Ignore Channels, Mailers and Jobs in SimpleCov

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -1,7 +1,7 @@
 require 'simplecov'
-SimpleCov.start 'rails'# do
-#  add_filter '/bin/'
-#  add_filter '/db/'
-#  add_filter '/spec/'
-#end
+SimpleCov.start 'rails' do
+  add_filter '/app/channels/'
+  add_filter '/app/mailers/'
+  add_filter '/app/jobs/'
+end
 
