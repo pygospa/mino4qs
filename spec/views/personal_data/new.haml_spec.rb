bb016fba25f3a9cab735c4527e6bf4713dedc664
Kannan Thambiah <kannan.thambiah@neuland-bfi.de>
2019-08-28 11:45:14 +0200

    Refactor view spec

--- spec/views/personal_data/new.haml_spec.rb
+++ spec/views/personal_data/new.haml_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'personal_data/new.haml', type: :view do
-  it 'displays a from to add personal data' do
+  it 'displays from for personal data' do
     render
     expect(rendered).to have_css("form.personal-data")
   end
 
-  it 'form contains a field to add birthday' do
-    render
-    expect(rendered).to have_field("birthday")
-  end
+  context 'form' do
+    it 'contains field for birthday' do
+      render
+      expect(rendered).to have_field("birthday")
+    end
 
-  it 'form contains a field to add gender' do
-    render
-    expect(render).to have_field("gender")
+    it 'contains a field for gender' do
+      render
+      expect(render).to have_field("gender")
+    end
   end
 end
 
