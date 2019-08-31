a2744aa26c9f42f66ae35ae0ceb19d340505be06
Kannan Thambiah <pygospa@gmail.com>
2019-08-31 13:15:09 +0200

    Add html response to create

--- app/controllers/personal_data_controller.rb
+++ app/controllers/personal_data_controller.rb
@@ -6,5 +6,7 @@ class PersonalDataController < ApplicationController
   end
 
   def create
+    flash[:success] = "Personal data successfully recorded"
+    redirect_to new_personal_datum_path
   end
 end
