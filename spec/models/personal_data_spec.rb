c8aa846d731fdac74e197581ed27caf133e5f66c
Kannan Thambiah <pygospa@gmail.com>
2019-09-01 16:00:06 +0200

    Add model spec outline

---
+++ spec/models/personal_data_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe PersonalData, type: :model do
+  it "is valid with valid attributes"
+  it "is not valid without birthday"
+  it "is not valid without gender"
+  it "is not valid without height"
+
+  context '#height' do
+    it "with no params returns height in centimeters"
+    it "with 'in: \"centimeters\"' returns height in cm"
+    it "with 'to_string: true' will return height in cm w/ unit"
+    it "with 'in: \"centimeters\", to_string: true' returns height in cm w/ unit"
+    it "with 'in: \"inches\"' returns height in in"
+    it "with 'in: \"inches\", to_string: true' returns height in in w/ unit"
+    it "with 'in: \"feet-and-inches\"' returns height in ft-in"
+    it "with 'in: \"feet-and-inches\", to_string: true' returns height in ft-in w/ unit"
+  end
+
+  context '#age' do
+    it "will return the current age for a given birthday"
+  end
+
+end
