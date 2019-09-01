b78de71ff2b35657af5ab830d4832ba48671e6fc
Kannan Thambiah <pygospa@gmail.com>
2019-09-01 23:31:26 +0200

    Fix validation spec

--- app/models/personal_data_set.rb
+++ app/models/personal_data_set.rb
@@ -4,20 +4,22 @@ class PersonalDataSet < ApplicationRecord
   validates :birthday, :gender, :height, presence: true
 
   def height(unit: 'centimeters', return_unit: false)
-    case unit
-    when 'centimeters'
-      return return_unit ? "#{@height} cm" : @height.to_i
-    when 'feet'
-      height_ft = (@height.to_i / 30.48).round(2)
-      return return_unit ? "#{height_ft} ft" : height_ft
-    when 'feet-and-inches'
-      height_ft = (@height.to_i / 30.48)
-      ft_part = height_ft.floor
-      in_part = ((height_ft - ft_part) * 12).floor
-      return "#{ft_part} ft #{in_part} in"
-    when 'inches'
-      height_in = (@height.to_i / 2.54).round(2)
-      return return_unit ? "#{height_in} in" : height_in
+    unless @height.nil? or @height.empty?
+      case unit
+      when 'centimeters'
+        return return_unit ? "#{@height} cm" : @height.to_i
+      when 'feet'
+        height_ft = (@height.to_i / 30.48).round(2)
+        return return_unit ? "#{height_ft} ft" : height_ft
+      when 'feet-and-inches'
+        height_ft = (@height.to_i / 30.48)
+        ft_part = height_ft.floor
+        in_part = ((height_ft - ft_part) * 12).floor
+        return "#{ft_part} ft #{in_part} in"
+      when 'inches'
+        height_in = (@height.to_i / 2.54).round(2)
+        return return_unit ? "#{height_in} in" : height_in
+      end
     end
   end
 
