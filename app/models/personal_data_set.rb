716c407d23d6ad519a7e8755f4b4ef31c793a457
Kannan Thambiah <pygospa@gmail.com>
2019-09-01 21:31:48 +0200

    Refactor

--- app/models/personal_data_set.rb
+++ app/models/personal_data_set.rb
@@ -4,14 +4,13 @@ class PersonalDataSet < ApplicationRecord
   validates :birthday, :gender, :height, presence: true
 
   def height(unit: 'centimeters', return_unit: false)
-    if unit == 'inches'
+    case unit
+    when 'centimeters'
+      return "#{@height} cm" if return_unit
+      return @height
+    when 'inches'
       return "#{(@height.to_i / 2.54).round(2)}"
     end
-
-    if return_unit
-      return "#{@height} cm"
-    end
-    return @height
   end
 
 end
