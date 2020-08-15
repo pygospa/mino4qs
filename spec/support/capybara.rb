cc92f5d36034d2c7c59c7fdc2bdb3b0d47e71903
Kannan Thambiah <pygospa@gmail.com>
2020-08-15 21:40:28 +0200

    Fix typo

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,9 +1,9 @@
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :racket_test
+    driven_by :rack_test
   end
 
-  config before(:each, type: :system, js: true) do
+  config.before(:each, type: :system, js: true) do
     driven_by :selenium_chrome_headless
   end
 end
