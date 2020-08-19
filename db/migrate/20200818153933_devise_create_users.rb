25324fb3f2d7a709f4b06ec5627235a254eb5419
Kannan Thambiah <pygospa@gmail.com>
2020-08-19 15:22:12 +0200

    Group authentication related columns at the end

--- db/migrate/20200818153933_devise_create_users.rb
+++ db/migrate/20200818153933_devise_create_users.rb
@@ -8,11 +8,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
 
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
       t.date :birthday
       t.column :sex, :sex
       t.decimal :height, precision: 3, scale: 2
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
