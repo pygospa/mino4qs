9dcfd6ec75a37692f21c635059eae52645a12ee1
Kannan Thambiah <pygospa@gmail.com>
2020-08-23 22:08:33 +0200

    Add scenario for locking users

--- spec/system/user_management_spec.rb
+++ spec/system/user_management_spec.rb
@@ -46,6 +46,45 @@ RSpec.describe 'Account management', type: :system do
     expect(page).to have_current_path '/'
     expect(page).to have_content "Signed in successfully"
   end
+
+  scenario 'locks out user after five failed login attempt and sends out unlocking mail' do
+    # given
+    user.skip_confirmation!
+    user.save!
+    expect(user).to be_confirmed
+    expect(db_entry_for user).to_not be_locked_at
+    wrong_password_user = user.dup
+    wrong_password_user.password = "#{user.password + 'abc' }"
+
+    # when
+    visit '/'
+    3.times do
+      sign_in wrong_password_user
+      expect(page).to have_content "Invalid Email or password"
+      expect(db_entry_for user).to_not be_locked_at
+    end
+
+    # then
+    sign_in wrong_password_user
+    expect(page).to have_content "You have one more attempt before your account is locked."
+    expect(db_entry_for user).to_not be_locked_at
+    sign_in wrong_password_user
+    expect(page).to have_content "Your account is locked."
+    expect(db_entry_for user).to be_locked_at
+
+    sign_in user
+    expect(page).to have_content "Your account is locked."
+
+    open_email user.email
+    expect(current_email).to have_content "Your account has been locked"
+    current_email.click_link "Unlock my account"
+
+    expect(page).to have_content "Your account has been unlocked successfully"
+    expect(page).to have_current_path '/users/sign_in'
+
+    sign_in user
+    expect(page).to have_content "Signed in successfully"
+  end
 end
 
 def sign_up(user)
@@ -66,4 +105,8 @@ def sign_in(user)
   fill_in "Email", with: user.email
   fill_in "Password", with: user.password
   click_button "Log in"
+end
+
+def db_entry_for user
+  User.where(email: user.email).first
 end
