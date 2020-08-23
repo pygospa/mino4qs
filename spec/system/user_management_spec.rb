92cf50017e74df2d199969cb93691e6c7a2de85b
Kannan Thambiah <pygospa@gmail.com>
2020-08-24 00:51:06 +0200

    Refactor scenarios
    1) Extracts steps into methods that group and describe them for better
       scenario readability and reusability
    2) Extract strings from methods (especially reused ones) and have them in
       a single place at the beginning (for easy changing, if needed)
    3) Remove technical aspects as scenarios should describe users view:
       - current_path: irrelevant for user, tested indirectly by the content
       of the page
       - user variable: never seen by the user, used in the test not the app
       - db state: irrelevant for user, tested indirectly by the content of
       the page

--- spec/system/user_management_spec.rb
+++ spec/system/user_management_spec.rb
@@ -5,91 +5,53 @@ require 'capybara/email/rspec'
 RSpec.describe 'Account management', type: :system do
   let(:navbar_xpath) { "//header/div[contains(@class, 'navbar')]" }
   let(:user)         { FactoryBot.build :user }
+  let(:successfully_confirmed) { 'Your email address has been successfully confirmed.' }
+  let(:successfully_signed_in) { 'Signed in successfully' }
+  let(:confirm_your_email)     { 'You have to confirm your email address before continuing' }
+  let(:invalid_credentials)    { 'Invalid Email or password' }
+  let(:one_more_attempt)       { 'You have one more attempt before your account is locked.' }
+  let(:account_is_locked)      { 'Your account is locked.' }
+  let(:account_unlocked)       { 'Your account has been unlocked successfully' }
 
   scenario 'allows new user to sign up for an account' do
-    # when
     visit '/'
-    sign_up user
-    open_email user.email
-    current_email.click_link "Confirm my account"
-
-    # then
-    expect(page).to have_current_path '/users/sign_in'
-    expect(page).to have_content "Your email address has been successfully confirmed."
+    fill_out_registration_for user
+    confirm_account_registration_mail_for user
+    expect(page).to have_content successfully_confirmed
   end
 
   scenario 'does not allow users with unconfirmed account to sign in' do
-    # given
-    user.save!
-    expect(user).to_not be_confirmed
-
-    # when
+    given_unconfirmed_account_for user
     visit '/'
     sign_in user
-
-    # then
-    expect(page).to have_current_path '/users/sign_in'
-    expect(page).to have_content "You have to confirm your email address before continuing"
+    expect(page).to have_content confirm_your_email
   end
 
   scenario 'allows users with confirmed account to log into their account' do
-    # given
-    user.skip_confirmation!
-    user.save!
-    expect(user).to be_confirmed
-
-    # when
+    given_confirmed_account_for user
     visit '/'
     sign_in user
-
-    # then
-    expect(page).to have_current_path '/'
-    expect(page).to have_content "Signed in successfully"
+    expect(page).to have_content successfully_signed_in
   end
 
   scenario 'locks out user after five failed login attempt and sends out unlocking mail' do
-    # given
-    user.skip_confirmation!
-    user.save!
-    expect(user).to be_confirmed
-    expect(db_entry_for user).to_not be_locked_at
-    wrong_password_user = user.dup
-    wrong_password_user.password = "#{user.password + 'abc' }"
-
-    # when
+    given_confirmed_account_for user
     visit '/'
-    3.times do
-      sign_in wrong_password_user
-      expect(page).to have_content "Invalid Email or password"
-      expect(db_entry_for user).to_not be_locked_at
-    end
 
-    # then
-    sign_in wrong_password_user
-    expect(page).to have_content "You have one more attempt before your account is locked."
-    expect(db_entry_for user).to_not be_locked_at
-    sign_in wrong_password_user
-    expect(page).to have_content "Your account is locked."
-    expect(db_entry_for user).to be_locked_at
+    five_failed_login_attempts_for user
 
     sign_in user
-    expect(page).to have_content "Your account is locked."
+    expect(page).to have_content account_is_locked
 
-    open_email user.email
-    expect(current_email).to have_content "Your account has been locked"
-    current_email.click_link "Unlock my account"
-
-    expect(page).to have_content "Your account has been unlocked successfully"
-    expect(page).to have_current_path '/users/sign_in'
+    unlock_account_via_email_for user
 
     sign_in user
-    expect(page).to have_content "Signed in successfully"
+    expect(page).to have_content successfully_signed_in
   end
 end
 
-def sign_up(user)
+def fill_out_registration_for(user)
   within(:xpath, navbar_xpath) { click_on 'Sign up' } # TODO: Should this really happen in navbar?
-  expect(page).to have_current_path '/users/sign_up'
   fill_in 'user[birthday]', with: user.birthday
   fill_in 'user[height]', with: user.height
   fill_in 'user[sex]', with: user.sex
@@ -99,14 +61,47 @@ def sign_up(user)
   click_button 'Sign up'
 end
 
+def five_failed_login_attempts_for(user)
+  user = generate_wrong_password_for user
+  5.times do |i|
+    sign_in user
+    expect(page).to have_content invalid_credentials if i <= 2
+    expect(page).to have_content one_more_attempt    if i == 3
+    expect(page).to have_content account_is_locked   if i == 4
+  end
+end
+
+def confirm_account_registration_mail_for(user)
+  open_email user.email
+  current_email.click_link "Confirm my account"
+end
+
+def unlock_account_via_email_for(user)
+  open_email user.email
+  current_email.click_link "Unlock my account"
+  expect(page).to have_content account_unlocked
+end
+
+def given_unconfirmed_account_for(user)
+  user.save!
+  expect(user).to_not be_confirmed
+end
+
+def given_confirmed_account_for(user)
+  user.skip_confirmation!
+  user.save!
+  expect(user).to be_confirmed
+end
+
+def generate_wrong_password_for(user)
+  wrong_password_user = user.dup
+  wrong_password_user.password = "#{user.password}-wrong"
+  return wrong_password_user
+end
+
 def sign_in(user)
   within(:xpath, navbar_xpath) { click_on 'Log in' } # TODO: Should this really happen in navbar?
-  expect(page).to have_current_path '/users/sign_in'
   fill_in "Email", with: user.email
   fill_in "Password", with: user.password
   click_button "Log in"
-end
-
-def db_entry_for user
-  User.where(email: user.email).first
 end
